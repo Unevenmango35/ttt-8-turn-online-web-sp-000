3a862beeddc50a2b8f3259df09c928a1d95d86f5
Rosie Choi <rosiechoimanso@gmail.com>
2020-06-12 02:40:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,3 +48,13 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
+end
