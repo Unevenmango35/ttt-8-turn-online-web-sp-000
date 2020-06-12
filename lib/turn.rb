719793f0e31c06dfb482e2a5fc620c5bb69bd51d
Rosie Choi <rosiechoimanso@gmail.com>
2020-06-12 02:16:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
 def position_taken? (board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     return false
@@ -22,9 +26,6 @@ def valid_move?(board, index)
   end
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
-end
 
 def move(board,index,value="X")
   board[index] = value
